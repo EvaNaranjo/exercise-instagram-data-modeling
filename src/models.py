d4de03b7637264fca012eb8bdfcb55fc97810c76
Eva Naranjo <evita.naranjo@gmail.com>
2022-02-05 12:10:34 +0000

    feat: init

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,23 +8,56 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    create = Column(DateTime)
+    update = Column(DateTime)
+    active = Boolean
 
-class Address(Base):
-    __tablename__ = 'address'
+class Album(Base):
+    __tablename__= "album"
+    id = Column(Integer, primary_key=True)
+    tittle = Column(String(250), nullable=False)
+    description = Column(String(250))
+    id_user = Column (Integer, ForeignKey("user.id"))
+    user = relationship(User)
+
+class Post(Base):
+    __tablename__= "post"
+    id = Column(Integer, primary_key=True)
+    tittle = Column(String(250), nullable=False)
+    description = Column(String(250))
+    create = Column(DateTime)
+    update = Column(DateTime)
+    id_user = Column (Integer, ForeignKey("user.id"))
+    user = relationship(User)
+    id_album = Column (Integer, ForeignKey("album.id"))
+    album = relationship(Album)
+    photo_url = Column(String(250))
+    type = Column(String(250))
+    total_likes = Column(Integer)
+    total_comments = Column(Integer)
+
+
+class Comment(Base):
+    __tablename__ = 'comment'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    content = Column(String(250))
+    post_date = Column(DateTime)
+    id_user = Column (Integer, ForeignKey("user.id"))
+    user = relationship(User)
+    id_post = Column (Integer, ForeignKey("post.id"))
+    post = relationship(Post)
+    
 
     def to_dict(self):
         return {}
